201c0a184a3046be6df69c849e308ef99fb6c931
Katykochte <kykochte@alaska.edu>
2025-09-24 19:32:50 -0800

    Refactored code

--- gameworld.gd
+++ gameworld.gd
@@ -6,9 +6,6 @@ var direction_map = {
 	"d": Vector2.RIGHT,
 }
 
-# Grid cell size (pixels per movement)
-var grid_size = 64  # Move exactly 64 pixels each time
-var is_moving = false
 
 func _ready():
 	# $Background.size = get_viewport().size
@@ -22,25 +19,13 @@ func _on_text_changed():
 		process_command(command) # run command
 
 func process_command(command):
-	if command.length() == 0 or is_moving:
+	if command.length() == 0:
 		return # nothing entered/currently moving
 	
 	if command in direction_map:
-		move_player_to_grid_pos(direction_map[command])
+		$Player.move_in_direction(direction_map[command])
 	else:
 		# loop through all commands if multiple
 		for cmd_char in command:
 			if cmd_char in direction_map:
-				move_player_to_grid_pos(direction_map[cmd_char])
-
-func move_player_to_grid_pos(direction):
-	is_moving = true
-	
-	# Calculate target position
-	var target_position = $Player.position + (direction * grid_size)
-	
-	# Move player to new position
-	$Player.position = target_position
-	
-	# Reset moving flag
-	is_moving = false
+				$Player.move_in_direction(direction_map[cmd_char])
--- player.gd
+++ player.gd
@@ -1,1 +1,15 @@
 extends CharacterBody2D
+
+# Movement settings
+var grid_size = 64
+var is_moving = false
+
+func move_in_direction(direction):
+	if is_moving:
+		return false  # Can't move right now
+	
+	is_moving = true
+	var target_position = position + (direction * grid_size)
+	position = target_position
+	is_moving = false
+	return true  # Move successful
